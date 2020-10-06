55ebf4de6d09ef75c7ec563adb85a5806ef547c9
Luke Irvin <luke.irvin21@gmail.com>
2020-10-06 08:30:36 -0500

    Added user agent header as per API instructions

--- gamepicker.js
+++ gamepicker.js
@@ -4,14 +4,20 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-fetch('https://api.rawg.io/api/games?page_size=1')
+fetch('https://api.rawg.io/api/games?page_size=1', {
+  method: 'Get',
+  headers: { 'User-Agent': 'Random Game Picker' },
+})
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     var count = data.count;
     var randomNumber = getRandomInt(count);
-    fetch(`https://api.rawg.io/api/games?page=${randomNumber}&page_size=1`)
+    fetch(`https://api.rawg.io/api/games?page=${randomNumber}&page_size=1`, {
+      method: 'Get',
+      headers: { 'User-Agent': 'Random Game Picker' },
+    })
       .then((response) => {
         return response.json();
       })
